6bf0e316dc43885c5761c4bcc50eacc7ded20a2a
Kok Kek <kokkekpek@gmail.com>
2020-07-06 10:17:38 +0300

    Change "update -y" to "update"

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 wget -qO - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88 -y
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt update -y
+sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo usermod -aG docker $USER
 
