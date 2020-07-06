7786b5e1bfd89c6f266e687d8f358b9d02a7f9f2
Kok Kek <kokkekpek@gmail.com>
2020-07-06 10:12:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Monitoring Installer
+Installer for [monitoring](https://github.com/kokkekpek/monitoring)
+
+* Create sed.json and htpasswd in one directory. See examples in repository
+* Run script
+```sh
+wget -qO - https://raw.githubusercontent.com/kokkekpek/monitoring-installer/master/install.sh | bash -
+```
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# System update
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+
+# GIT
+sudo apt-add-repository ppa:git-core/ppa -y
+sudo apt update
+sudo apt install git -y
+
+# Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+wget -qO - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 -y
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+sudo usermod -aG docker $USER
+
+# Clone repository
+git clone https://github.com/kokkekpek/monitoring.git
+
+# Copy configs
+cp sed.json monitoring
+cp htpasswd monitoring/config/.htpasswd
+
+# Install and up
+cd monitoring || return
+docker run -it --rm --name script -v "$PWD":/usr/src/app -w /usr/src/app node:14.5-alpine node install
+docker-compose up -d
