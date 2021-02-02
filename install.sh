54ae10fce7d56d4e828ff12080acf5d36be8d8b4
Nikita <66723320+kokkekpek@users.noreply.github.com>
2021-02-03 01:38:52 +0300

    Add permission to docker-compose

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ sudo usermod -aG docker $USER
 
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo u+x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Clone repository
