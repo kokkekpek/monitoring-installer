0b363bcd8bdc33bd3c613b65be3ba1a4b8bcd5ba
Kok Kek <kokkekpek@gmail.com>
2021-04-26 02:41:09 +0300

    Fix. Add chmod

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sudo apt install apache2-utils -y
 
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo u+x /usr/local/bin/docker-compose
+sudo chmod u+x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Clone repository
